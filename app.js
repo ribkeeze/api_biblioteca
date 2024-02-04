6e249301336e7362606eeff36a7263f69b137f45
ribkeeze <ribkeeze@gmail.com>
2024-02-04 01:35:01 -0300

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express");
+
+const { auth } = require("express-oauth2-jwt-bearer");
+const errorHandler = require("./middleware/errorHandler");
+
+// Configuracion Middleware con el Servidor de Autorización
+const autenticacion = auth({
+  audience: "http://localhost:3000/api/productos",
+  issuerBaseURL: "https://dev-utn-frc-iaew.auth0.com/",
+  tokenSigningAlg: "RS256",
+});
+
 const app = express();
 app.use(express.json());
 
 // Importamos el Router de Libros
 const librosRouter = require("./routes/libros");
 
-// Importamos el Middleware Error Handler
-const errorHandler = require("./middleware/errorHandler");
-
-app.use("/libros", librosRouter);
+//Configuramos el middleware de autenticacion
+app.use("/libros", autenticacion, librosRouter);
 
 app.use(errorHandler);
 
--- routes/libros.js
+++ routes/libros.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Libro = require("../models/Libro");
+// Importamos la librería para validar scopes
+const { requiredScopes } = require("express-oauth2-jwt-bearer");
 // Ruta para obtener todos los libros
-router.get("/", async (req, res) => {
+router.get("/", requiredScopes("read:libros"), async (req, res) => {
   try {
     const libros = await Libro.find();
     res.json(libros);
@@ -11,7 +13,7 @@ router.get("/", async (req, res) => {
   }
 });
 // Ruta para crear un nuevo Libro
-router.post("/", async (req, res) => {
+router.post("/", requiredScopes("write:libros"), async (req, res) => {
   try {
     const nuevoLibro = new Libro(req.body);
     await nuevoLibro.save();
@@ -21,7 +23,7 @@ router.post("/", async (req, res) => {
   }
 });
 // Ruta para actualizar un Libro existente
-router.put("/:id", async (req, res) => {
+router.put("/:id", requiredScopes("write:libros"), async (req, res) => {
   try {
     const Libro = await Libro.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -32,7 +34,7 @@ router.put("/:id", async (req, res) => {
   }
 });
 // Ruta para eliminar un Libro
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", requiredScopes("write:libros"), async (req, res) => {
   try {
     await Libro.findByIdAndDelete(req.params.id);
     res.json({ message: "Libro eliminado correctamente" });
@@ -40,5 +42,4 @@ router.delete("/:id", async (req, res) => {
     res.status(500).json({ error: "Error al eliminar el Libro" });
   }
 });
-
 module.exports = router;
